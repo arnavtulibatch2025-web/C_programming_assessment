4fb78b55489dbff06f1c28b3437ecbb01b8652d5
arnavtulibatch2025-web <arnav.tuli.batch2025@sitnagpur.siu.edu.in>
2025-11-20 12:47:30 +0530

    asd

---
+++ C_programming_assessment/branching and looping/digitprocessing.c
@@ -0,0 +1,20 @@
+// Arnav Tuli 25070521070
+
+
+#include <stdio.h>
+
+void main() {
+  int n, sum = 0, counter = 0;
+  scanf("%d", &n);
+  while (n != 0) {
+    counter++;
+    sum = sum + n % 10;
+    n = n / 10;
+  }
+  if (counter == 5) {
+    printf("%d", sum);
+
+  } else {
+    printf("Invalid input");
+  }
+}
---
+++ C_programming_assessment/operators/relational.c
@@ -0,0 +1,39 @@
+// Arnav Tuli 25070521070
+
+
+
+#include <stdio.h>
+
+int main(){
+	int inp1 , inp2;
+
+	printf("Enter two numbers: ");
+	scanf("%d %d" , &inp1,&inp2);
+
+	printf("%d < %d: " , inp1 , inp2);
+	printf("%s" ,inp1 < inp2 ? "True" : "False");
+	printf("\n");
+
+	printf("%d <= %d: ", inp1 , inp2);
+	printf("%s" ,inp1 <= inp2 ? "True" : "False");
+	printf("\n");
+
+	printf("%d > %d: ", inp1 , inp2);
+	printf("%s" ,inp1 > inp2 ? "True" : "False");
+	printf("\n");
+
+	printf("%d >= %d: ", inp1 , inp2);
+	printf("%s" ,inp1 >= inp2 ? "True" : "False");
+	printf("\n");
+
+	printf("%d == %d: ", inp1 , inp2);
+	printf("%s" ,inp1 == inp2 ? "True" : "False");
+	printf("\n");
+
+	printf("%d != %d: ", inp1 , inp2);
+	printf("%s" ,inp1 != inp2 ? "True" : "False");
+	printf("\n");
+
+	return 0;
+
+}
